b3abddac68ddeb6f529c9402763aae43e8da245a
HelloToCPPALL <vukpavlovic007@gmail.com>
2021-01-23 22:10:23 +0100

    Boze sacuvaj

---
+++ slider.js
@@ -0,0 +1,11 @@
+var mySwiper = new Swiper('.swiper-container', {
+   // Optional parameters
+   direction: 'horizontal',
+   slidesPerView: '2.6',
+ 
+   // Navigation arrows
+   navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+   }
+ })
