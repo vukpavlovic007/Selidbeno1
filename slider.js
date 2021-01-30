4fab1479156f517d8c1abedbc23a23c1e3a07fca
HelloToCPPALL <vukpavlovic007@gmail.com>
2021-01-30 20:08:32 +0100

    aha

--- slider.js
+++ slider.js
@@ -7,5 +7,26 @@ var mySwiper = new Swiper('.swiper-container', {
    navigation: {
      nextEl: '.swiper-button-next',
      prevEl: '.swiper-button-prev',
+   },
+
+   // Responsive breakpoints
+   breakpoints: {
+    0: {
+      slidesPerView: 1.3,
+      spaceBetween: 5
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2.2,
+      spaceBetween: 20
+     },
+     900: {
+      slidesPerView: 3,
+      spaceBetween: 30
+     },
+     1500: {
+      slidesPerView: 3.7,
+      spaceBetween: 30
+     }
    }
  })
